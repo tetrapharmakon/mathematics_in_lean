e82d071302f0122ccfd31666d4c0ad38cd744494
fouche <folore@ttu.ee>
2025-08-21 19:23:19 +0300

    work on ch03s05

--- MIL copy/C03_Logic/S05_Disjunction.lean
+++ MIL copy/C03_Logic/S05_Disjunction.lean
@@ -23,8 +23,12 @@ example (h : y < -1) : y > 0 ∨ y < -1 :=
 
 example : x < |y| → x < y ∨ x < -y := by
   rcases le_or_gt 0 y with h | h
+  -- P ∨ Q → R requires a proof of
+  -- P → R `and` a proof of Q → R
+  -- P → R:
   · rw [abs_of_nonneg h]
     intro h; left; exact h
+  -- Q → R:
   · rw [abs_of_neg h]
     intro h; right; exact h
 
@@ -58,19 +62,76 @@ example : x < |y| → x < y ∨ x < -y := by
 namespace MyAbs
 
 theorem le_abs_self (x : ℝ) : x ≤ |x| := by
-  sorry
+  rcases le_or_gt 0 x with h | h
+  rw [abs_of_nonneg h]
+  rw [abs_of_neg h]
+  linarith
 
 theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
-  sorry
+  rcases le_or_gt 0 x with h | h
+  rw [abs_of_nonneg h]
+  linarith
+  rw [abs_of_neg h]
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+  rcases le_or_gt 0 (x + y) with h | h
+  rw [abs_of_nonneg h]
+  apply add_le_add (le_abs_self x) (le_abs_self y)
+  rw [abs_of_neg h]
+  simp only [neg_add]
+  apply add_le_add (neg_le_abs_self x) (neg_le_abs_self y)
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+  constructor
+  · rcases le_or_gt 0 y with h | h
+    rw [abs_of_nonneg h]
+    intro x_le_y
+    exact Or.inl x_le_y
+    rw [abs_of_neg h]
+    intro x_le_my
+    exact Or.inr x_le_my
+  · rcases le_or_gt 0 y with h | h
+    · rw [abs_of_nonneg h]
+      intro bau
+      rcases bau with a | b
+      exact a
+      have : -y ≤ y := by
+        apply (le_trans (neg_nonpos_of_nonneg h) h)
+      apply lt_of_lt_of_le b this
+    · rw [abs_of_neg h]
+      intro miao
+      rcases miao with a | b
+      have : x < -y := by
+        apply (lt_trans a (lt_trans h (neg_pos_of_neg h)))
+      exact this
+      exact b
+
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+  constructor
+  · intro bau
+    rcases le_or_gt 0 x with h | h
+    · rw [abs_of_nonneg h] at bau
+      constructor
+      · have fst : -y < -x := by apply neg_lt_neg bau
+        have snd : -x ≤ 0 := by apply neg_nonpos_of_nonneg h
+        have trd : -x < x := by sorry
+        exact lt_trans fst trd
+      · exact bau
+    · rw [abs_of_neg h] at bau
+      constructor
+      · apply neg_lt_of_neg_lt bau
+      · have : x < -x := by apply lt_trans h (neg_pos_of_neg h)
+        apply lt_trans this bau
+  · rintro ⟨pio,pao⟩
+    rcases le_or_gt 0 x with h | h
+    rw [abs_of_nonneg]
+    exact pao
+    exact h
+    rw [abs_of_neg]
+    exact neg_lt_of_neg_lt pio
+    exact h
+
 
 end MyAbs
 
@@ -125,4 +186,3 @@ example (P : Prop) : ¬¬P → P := by
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
   sorry
-
